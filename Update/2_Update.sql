73d95ef0d43b94957d4a7768f59f38481c850979
Bes666 <mr-fear@ya.ru>
2011-05-04 14:37:53 +0400

    Some Updates

---
+++ Update/2_Update.sql
@@ -0,0 +1,32 @@
+UPDATE  `item_template` SET  `Material` =  '4' WHERE  `item_template`.`entry` =52316;
+UPDATE  `item_template` SET  `unk0` =  '-1' WHERE  `item_template`.`entry` =54601;
+UPDATE  `item_template` SET  `unk0` =  '-1' WHERE  `item_template`.`entry` =54602;
+UPDATE  `item_template` SET  `unk0` =  '-1' WHERE  `item_template`.`entry` =54603;
+UPDATE  `item_template` SET  `unk0` =  '-1' WHERE  `item_template`.`entry` =54604;
+UPDATE  `item_template` SET  `unk0` =  '-1' WHERE  `item_template`.`entry` =54605;
+DELETE FROM `item_enchantment_template` WHERE `item_enchantment_template`.`entry` = 130 AND `item_enchantment_template`.`ench` = 0
+DELETE FROM `item_enchantment_template` WHERE `item_enchantment_template`.`entry` = 442 AND `item_enchantment_template`.`ench` = 0
+DELETE FROM `item_enchantment_template` WHERE `item_enchantment_template`.`entry` = 415 AND `item_enchantment_template`.`ench` = 0
+DELETE FROM `item_enchantment_template` WHERE `item_enchantment_template`.`entry` = 441 AND `item_enchantment_template`.`ench` = 0
+DELETE FROM `item_enchantment_template` WHERE `item_enchantment_template`.`entry` = 450 AND `item_enchantment_template`.`ench` = 0
+DELETE FROM `item_enchantment_template` WHERE `item_enchantment_template`.`entry` = 451 AND `item_enchantment_template`.`ench` = 0
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '125', '2');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '127', '2.1');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '123', '5.9');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '122', '6.3');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '120', '6.5');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '118', '6.3');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '138', '9.1');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '130', '9.5');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '114', '4.5');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '134', '4.1');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '121', '6.5');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '124', '2.1');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '128', '2.1');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '133', '4.5');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '131', '6.5');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '132', '5.9');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '129', '4.5');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '135', '3.6');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '137', '4.2');
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES ('442', '136', '3.7');
